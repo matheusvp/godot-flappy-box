ed87d81bd7e0449ff605f22611f4b0372a06537a
matheusvp <matheus.vp01@gmail.com>
2026-03-17 21:54:19 -0300

    fixed player animations

--- scripts/player.gd
+++ scripts/player.gd
@@ -19,6 +19,11 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	
+	if velocity.y > 0:
+		animation.play("falling")
+	else:
+		animation.play("jumping")
+	
 	move_and_slide()
 
 func toggle_movent():
